759fd80a29f7109512b2e5d9b895e0374c646c6f
Khalid Arnob <83981477+en-arnob@users.noreply.github.com>
2023-02-01 23:35:48 +0600

    Products Pane, Footer

--- app/components/FooterBanner.jsx
+++ app/components/FooterBanner.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const FooterBanner = () => {
-  return <div>FooterBanner</div>;
+const FooterBanner = ({ bannerD }) => {
+  return (
+    <div className='footer-banner-container'>
+      {console.log(bannerD)}
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{bannerD.discount}</p>
+          <h3>{bannerD.largeText1}</h3>
+          <h3>{bannerD.largeText2}</h3>
+          <p>{bannerD.saleTime}</p>
+        </div>
+        <div className='right'>
+          <p>{bannerD.smallText}</p>
+          <h3>{bannerD.midText}</h3>
+          <p>{bannerD.desc}</p>
+          <Link href={`/product/${bannerD.product}`}>
+            <button type='button'>{bannerD.buttonText}</button>
+          </Link>
+        </div>
+        <img src={urlFor(bannerD.image)} className='footer-banner-image' />
+      </div>
+    </div>
+  );
 };
 
 export default FooterBanner;
--- app/pages/index.js
+++ app/pages/index.js
@@ -12,9 +12,11 @@ const Home = ({ products, bannerData }) => {
         <p>Headphones</p>
       </div>
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner bannerD={bannerData.length && bannerData[0]} />
     </>
   );
 };
