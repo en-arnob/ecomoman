7b49b48619ccbcbfbbd87264498ed467388537cf
Khalid Arnob <83981477+en-arnob@users.noreply.github.com>
2023-02-02 01:43:54 +0600

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
+import { FiShoppingBag } from "react-icons/fi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Ecomoman Store</Link>
+      </p>
+      <button type='button' className='cart-icon' onClick=''>
+        <FiShoppingBag />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
