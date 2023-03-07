a2d8b8edb48a7fc2a4766ffc459d3bc4328934f2
Khalid Arnob <en.arnob@gmail.com>
2023-03-08 00:41:16 +0600

    cart initial

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
 const Cart = () => {
   return <div>Cart</div>;
 };
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
+import { Cart } from "./";
+
+import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <p className='logo '>
@@ -10,10 +14,15 @@ const Navbar = () => {
           <h3>Ecomoman</h3>
         </Link>
       </p>
-      <button type='button' className='cart-icon' onClick=''>
+      <button
+        type='button'
+        className='cart-icon'
+        onClick={() => setShowCart(true)}
+      >
         <FiShoppingBag />
-        <span className='cart-item-qty mb-10'>1</span>
+        <span className='cart-item-qty mb-10'>{totalQuantities}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };
