08b260709b65184148ca0d12824106446e2309f3
Khalid Arnob <83981477+en-arnob@users.noreply.github.com>
2023-04-11 16:41:25 +0600

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -115,7 +115,7 @@ const Footer = () => {
         </div>
         <div className='grid justify-center pt-6 lg:justify-between'>
           <div className='flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6'>
-            <span>©2023 All rights reserved</span>
+            <span>©2023 Khalid Arnob, All rights reserved</span>
             <a rel='noopener noreferrer' href='#'>
               <span>Privacy policy</span>
             </a>
